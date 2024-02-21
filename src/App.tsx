819313511d63a0367d1b49b4a7daa7ae9e3fc262
tamir.rust <timorust1@gmail.com>
2024-02-21 11:25:49 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@nextui-org/react"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button color="primary">Button!!!</Button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/app/store.ts
@@ -0,0 +1,18 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
+
+export const store = configureStore({
+  reducer: {},
+})
+
+export type AppStore = typeof store
+
+export type AppDispatch = AppStore["dispatch"]
+export type RootState = ReturnType<typeof store.getState>
+
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
