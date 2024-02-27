acaf104b192db914a4ef56da96e60516dd6eb384
tamir.rust <timorust1@gmail.com>
2024-02-27 09:18:16 +0200

    Auth comp

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,3 +1,30 @@
+import { Card, CardBody, Tab, Tabs } from "@nextui-org/react"
+import { useState } from "react"
+
 export const Auth = () => {
-  return <div>Auth</div>
+  const [selected, setSelected] = useState("login")
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="flex flex-col">
+        <Card className="max-w-full w-[340px] h-[450px]">
+          <CardBody className="overflow-hidden">
+            <Tabs
+              fullWidth
+              size="md"
+              selectedKey={selected}
+              onSelectionChange={key => setSelected(key as string)}
+            >
+              <Tab key="login" title="Login">
+                Login
+              </Tab>
+              <Tab key="sign-up" title="Registration">
+                Registration
+              </Tab>
+            </Tabs>
+          </CardBody>
+        </Card>
+      </div>
+    </div>
+  )
 }
