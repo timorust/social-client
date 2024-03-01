891d008f3c588538422db787319e486c61207f9d
tamir.rust <timorust1@gmail.com>
2024-03-01 03:30:41 +0200

    AuthGuard

---
+++ src/features/user/authGuard.tsx
@@ -0,0 +1,11 @@
+import { Spinner } from "@nextui-org/react"
+import { useCurrentQuery } from "../../app/services/userApi"
+
+export const AuthGuard = ({ children }: { children: JSX.Element }) => {
+  const { isLoading } = useCurrentQuery()
+
+  if (isLoading) {
+    return <Spinner />
+  }
+  return children
+}
