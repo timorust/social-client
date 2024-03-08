f9462ef37e2c631533039e4ec088895b954844fa
tamir.rust <timorust1@gmail.com>
2024-03-08 09:29:29 +0200

    user card  user-info

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,3 +1,29 @@
+import {
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Card as NextUiCard,
+  Spinner,
+  User,
+} from "@nextui-org/react"
+import { useState } from "react"
+import { RiDeleteBinLine } from "react-icons/ri"
+import { useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { useDeleteCommentMutation } from "../../app/services/commentsApi"
+import {
+  useLikePostMutation,
+  useUnLikePostMutation,
+} from "../../app/services/likesApi"
+import {
+  useDeletePostMutation,
+  useLazyGetAllPostsQuery,
+  useLazyGetPostByIdQuery,
+} from "../../app/services/postsApi"
+import { selectCurrent } from "../../features/user/userSlice"
+import { formatToClientDate } from "../../utils/toClientDate"
+import { Typography } from "../typography"
+
 type Props = {
   avatarUrl: string
   name: string
@@ -11,6 +37,63 @@ type Props = {
   cardFor: "comment" | "post" | "current-post"
   likedByUser?: boolean
 }
-export const Card = () => {
-  return <div></div>
+export const Card: React.FC<Props> = ({
+  avatarUrl = "",
+  name = "",
+  authorId = "",
+  content = "",
+  commentId = "",
+  likesCount = 0,
+  commentsCount = 0,
+  createdAt,
+  id = "",
+  cardFor = "post",
+  likedByUser = false,
+}) => {
+  const [likePost] = useLikePostMutation()
+  const [unlikePost] = useUnLikePostMutation()
+  const [triggerGetAllPosts] = useLazyGetAllPostsQuery()
+  const [triggerGetPostById] = useLazyGetPostByIdQuery()
+  const [deletePost, deletePostStatus] = useDeletePostMutation()
+  const [deleteComment, deleteCommentStatus] = useDeleteCommentMutation()
+  const [error, setError] = useState("")
+  const navigate = useNavigate()
+  const currentUser = useSelector(selectCurrent)
+
+  return (
+    <NextUiCard className="mb-5">
+      <CardHeader className="justify-between items-center bg-transparent">
+        <Link to={`/users/${authorId}`}>
+          <User
+            name={name}
+            className="text-small font-semibold leading-non text-default-600"
+            avatarUrl={avatarUrl}
+            description={createdAt && formatToClientDate(createdAt)}
+          />
+        </Link>
+
+        {authorId === currentUser?.id && (
+          <div className="cursor-pointer">
+            {deletePostStatus.isLoading || deleteCommentStatus.isLoading ? (
+              <Spinner />
+            ) : (
+              <RiDeleteBinLine />
+            )}
+          </div>
+        )}
+      </CardHeader>
+
+      <CardBody className="px-3 py-2 mb-5">
+        <Typography>{content}</Typography>
+      </CardBody>
+
+      {cardFor !== "comment" && (
+        <CardFooter className="gap-3">
+          <div className="flex gap-5 items-center">
+            <div></div>
+          </div>
+        </CardFooter>
+      )}
+    </NextUiCard>
+  )
 }
--- src/utils/toClientDate.ts
+++ src/utils/toClientDate.ts
@@ -1,4 +1,4 @@
-export const FormatToClientDate = (date?: Date) => {
+export const formatToClientDate = (date?: Date) => {
   if (!date) {
     return ""
   }
