65aee401a9418dc5754b2cc10326e3d2262f3e91
tamir.rust <timorust1@gmail.com>
2024-03-14 20:18:25 +0200

    go back

---
+++ src/components/go-back/index.tsx
@@ -0,0 +1,21 @@
+import { FaRegArrowAltCircleLeft } from "react-icons/fa"
+import { useNavigate } from "react-router-dom"
+
+const GoBack = () => {
+  const navigate = useNavigate()
+
+  const handleGoBack = () => {
+    navigate(-1)
+  }
+  return (
+    <div
+      onClick={handleGoBack}
+      className="text-default-500 flex items-center gap-2 mb-10 cursor-pointer"
+    >
+      <FaRegArrowAltCircleLeft />
+      Go back
+    </div>
+  )
+}
+
+export default GoBack
--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,3 +1,40 @@
+import { useParams } from "react-router-dom"
+import { useGetPostByIdQuery } from "../../app/services/postsApi"
+import { Card } from "../../components/card"
+import GoBack from "../../components/go-back"
+
 export const CurrentPost = () => {
-  return <div>CurrentPost</div>
+  const params = useParams<{ id: string }>()
+
+  const { data } = useGetPostByIdQuery(params?.id ?? "")
+
+  if (!data) return <h2>Post not found</h2>
+
+  const {
+    content,
+    id,
+    authorId,
+    comments,
+    likes,
+    author,
+    likedByUser,
+    createdAt,
+  } = data
+  return (
+    <>
+      <GoBack />
+      <Card
+        cardFor="current-post"
+        avatarUrl={author.avatarUrl ?? ""}
+        content={content}
+        name={author.name ?? ""}
+        likesCount={likes.length}
+        commentsCount={comments.length}
+        authorId={authorId}
+        id={id}
+        likedByUser={likedByUser}
+        createdAt={createdAt}
+      />
+    </>
+  )
 }
